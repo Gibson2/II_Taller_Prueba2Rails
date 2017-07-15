22bb3a0abbefa26f5cab9700ae330edd9744c263
Guillermo <gibsonguillermoarturo@gmail.com>
2017-07-14 20:34:06 -0400

    Creando productos premium

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :price, presence: true
+  before_save :premium_false
+
+  def productwithprice
+
+  	if pr.nil?
+
+  	end
+
+  end
+
+
+
 end
